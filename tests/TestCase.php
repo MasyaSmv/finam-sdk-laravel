aa1b75e2ff6420e27578265d6e872883913b665d
MasyaSm <kuvaldamax@gmail.com>
2026-04-03 14:19:01 +0300

    Add Testbench compatibility shim for prefer-lowest

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,6 +8,10 @@ use Illuminate\Foundation\Application;
 use Illuminate\Contracts\Config\Repository as ConfigRepository;
 use MasyaSmv\FinamSdk\FinamSdkServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionMethod;
+use ReflectionProperty;
 
 /**
  * Базовый TestCase для пакета.
@@ -17,6 +21,19 @@ use Orchestra\Testbench\TestCase as Orchestra;
  */
 abstract class TestCase extends Orchestra
 {
+    /**
+     * Compatibility shim for older orchestra/testbench releases pulled by prefer-lowest.
+     *
+     * @return array{class: array<string, list<string>>, method: array<string, list<string>>}
+     */
+    public function getAnnotations(): array
+    {
+        return [
+            'class' => $this->parseAnnotations((new ReflectionClass(static::class))->getDocComment() ?: ''),
+            'method' => $this->parseMethodAnnotations(),
+        ];
+    }
+
     /**
      * Регистрируем провайдер нашего пакета в тестовом приложении.
      *
@@ -49,5 +66,50 @@ abstract class TestCase extends Orchestra
         $config->set('finam.http.retries', 1);
         $config->set('finam.http.retry_delay_ms', 10);
         $config->set('finam.http.user_agent', 'finam-sdk-tests');
+    }
+
+    /**
+     * @return array<string, list<string>>
+     */
+    private function parseMethodAnnotations(): array
+    {
+        try {
+            $method = new ReflectionMethod($this, $this->currentTestMethodName());
+        } catch (ReflectionException) {
+            return [];
+        }
+
+        return $this->parseAnnotations($method->getDocComment() ?: '');
+    }
+
+    /**
+     * @return array<string, list<string>>
+     */
+    private function parseAnnotations(string $docComment): array
+    {
+        $annotations = [];
+
+        preg_match_all('/@([A-Za-z_\\\\-]+)(?:[ \t]+([^\r\n*]+))?/', $docComment, $matches, PREG_SET_ORDER);
+
+        foreach ($matches as $match) {
+            $name = $match[1];
+            $value = trim($match[2] ?? '');
+
+            $annotations[$name] ??= [];
+            $annotations[$name][] = $value;
+        }
+
+        return $annotations;
+    }
+
+    private function currentTestMethodName(): string
+    {
+        $property = new ReflectionProperty(Orchestra::class, 'name');
+        $property->setAccessible(true);
+
+        /** @var string $name */
+        $name = $property->getValue($this);
+
+        return $name;
     }
 }
